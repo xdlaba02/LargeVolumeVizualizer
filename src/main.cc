009446592b0beaf1a799ea78c149a269808b1e1e
Drahomír Dlabaja <xdlaba02@stud.fit.vutbr.cz>
2021-11-14 18:58:25 +0100

    vectorization

--- src/main.cc
+++ src/main.cc
@@ -6,13 +6,18 @@
 
 #include <GLFW/glfw3.h>
 
-#include <experimental/simd>
+#include <Vc/Vc>
 
 #include <iostream>
 #include <vector>
 #include <cmath>
 
-using simdline = std::experimental::native_simd;
+using Vc::float_v;
+using Vc::float_m;
+
+constexpr std::size_t simdlen = float_v::size();
+
+using uint32_t_v = Vc::SimdArray<uint32_t, simdlen>;
 
 template <typename ColorType>
 class ColorGradient1D {
@@ -48,38 +53,34 @@ public:
 };
 
 
-bool intersect_aabb_ray(const glm::vec3& origin, const glm::vec3& direction, const glm::vec3 &min, const glm::vec3 &max, float& tMin, float& tMax) {
-  glm::vec3 inv_direction = glm::vec3{1, 1, 1} / direction;
+void intersect_aabb_rays_single_origin(const glm::vec3& origin, glm::vec<3, float_v> ray_directions, const glm::vec3 &min, const glm::vec3 &max, float_v& tmins, float_v& tmaxs) {
+  ray_directions = glm::vec<3, float_v>{1, 1, 1} / ray_directions;
 
-  tMin = -std::numeric_limits<float>::infinity();
-  tMax = std::numeric_limits<float>::infinity();
+  tmins = -std::numeric_limits<float>::infinity();
+  tmaxs = std::numeric_limits<float>::infinity();
 
-  for (size_t i = 0; i < 3; ++i) {
-    float t0, t1;
+  for (std::size_t i = 0; i < 3; ++i) {
+    float_v t0 = (min[i] - origin[i]) * ray_directions[i];
+    float_v t1 = (max[i] - origin[i]) * ray_directions[i];
 
-    if (inv_direction[i] >= 0.f) {
-      t0 = (min[i] - origin[i]) * inv_direction[i];
-      t1 = (max[i] - origin[i]) * inv_direction[i];
-    }
-    else {
-      t1 = (min[i] - origin[i]) * inv_direction[i];
-      t0 = (max[i] - origin[i]) * inv_direction[i];
-    }
+    float_m swap_mask = ray_directions[i] < 0.f;
 
-    tMin = t0 > tMin ? t0 : tMin;
-    tMax = t1 < tMax ? t1 : tMax;
+    t0(swap_mask) = t0 + t1;
+    t1(swap_mask) = t0 - t1;
+    t0(swap_mask) = t0 - t1;
+
+    tmins(t0 > tmins) = t0;
+    tmaxs(t1 < tmaxs) = t1;
   }
-
-  return tMax + std::numeric_limits<float>::epsilon() >= tMin;
 }
 
 int main(int argc, char *argv[]) {
-
   glm::vec3 volume_min {-.5, -.5, -.5};
   glm::vec3 volume_max {+.5, +.5, +.5};
 
-  constexpr size_t width = 1920;
-  constexpr size_t height = 1080;
+  constexpr std::size_t width = 1920;
+  constexpr std::size_t height = 1080;
+  constexpr float stepsize = 0.01f;
 
   std::vector<uint8_t> raster(width * height * 3);
 
@@ -104,30 +105,36 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  auto sampler = [&volume](float x, float y, float z) {
-    x *= 255;
-    y *= 255;
-    z *= 255;
+  auto sampler = [&volume](float_v xs, float_v ys, float_v zs, float_m mask) {
+    xs *= 255.f;
+    ys *= 255.f;
+    zs *= 255.f;
 
-    size_t pix_x = x;
-    size_t pix_y = y;
-    size_t pix_z = z;
+    uint32_t_v pix_xs = xs;
+    uint32_t_v pix_ys = ys;
+    uint32_t_v pix_zs = zs;
 
-    float frac_x = x - pix_x;
-    float frac_y = y - pix_y;
-    float frac_z = z - pix_z;
+    float_v frac_xs = xs - pix_xs;
+    float_v frac_ys = ys - pix_ys;
+    float_v frac_zs = zs - pix_zs;
 
-    float acc0 = volume(pix_x, pix_y,     pix_z    ) * (1 - frac_x) + volume(pix_x + 1, pix_y    , pix_z    ) * frac_x;
-    float acc1 = volume(pix_x, pix_y + 1, pix_z    ) * (1 - frac_x) + volume(pix_x + 1, pix_y + 1, pix_z    ) * frac_x;
-    float acc2 = volume(pix_x, pix_y,     pix_z + 1) * (1 - frac_x) + volume(pix_x + 1, pix_y    , pix_z + 1) * frac_x;
-    float acc3 = volume(pix_x, pix_y + 1, pix_z + 1) * (1 - frac_x) + volume(pix_x + 1, pix_y + 1, pix_z + 1) * frac_x;
+    float_v accs[4];
 
-    acc0 = acc0 * (1 - frac_y) + acc1 * frac_y;
-    acc1 = acc2 * (1 - frac_y) + acc3 * frac_y;
+    for (uint32_t k = 0; k < simdlen; k++) {
+      if (mask[k]) {
+        accs[0][k] = volume(pix_xs[k], pix_ys[k],     pix_zs[k]    ) * (1 - frac_xs[k]) + volume(pix_xs[k] + 1, pix_ys[k]    , pix_zs[k]    ) * frac_xs[k];
+        accs[1][k] = volume(pix_xs[k], pix_ys[k] + 1, pix_zs[k]    ) * (1 - frac_xs[k]) + volume(pix_xs[k] + 1, pix_ys[k] + 1, pix_zs[k]    ) * frac_xs[k];
+        accs[2][k] = volume(pix_xs[k], pix_ys[k],     pix_zs[k] + 1) * (1 - frac_xs[k]) + volume(pix_xs[k] + 1, pix_ys[k]    , pix_zs[k] + 1) * frac_xs[k];
+        accs[3][k] = volume(pix_xs[k], pix_ys[k] + 1, pix_zs[k] + 1) * (1 - frac_xs[k]) + volume(pix_xs[k] + 1, pix_ys[k] + 1, pix_zs[k] + 1) * frac_xs[k];
+      }
+    }
 
-    acc0 = acc0 * (1 - frac_z) + acc1 * frac_z;
+    accs[0] = accs[0] * (1 - frac_ys) + accs[1] * frac_ys;
+    accs[1] = accs[2] * (1 - frac_ys) + accs[3] * frac_ys;
 
-    return acc0;
+    accs[0] = accs[0] * (1 - frac_zs) + accs[1] * frac_zs;
+
+    return accs[0];
   };
 
   ColorGradient1D<glm::vec4> transfer {};
@@ -138,6 +145,12 @@ int main(int argc, char *argv[]) {
   transfer.setColor(59.f,  {0.0f, 0.00f, 0.0f, 0.5f});
   transfer.setColor(60.f,  {0.7f, 0.35f, 0.0f, 0.5f});
   transfer.setColor(255.f, {0.9f, 0.55f, 0.0f, 0.8f});
+
+  uint32_t_v offsets {};
+
+  for (std::size_t k = 0; k < simdlen; k++) {
+    offsets[k] = k * 3;
+  }
 
   float time = 0.f;
   while (!glfwWindowShouldClose(window)) {
@@ -150,41 +163,68 @@ int main(int argc, char *argv[]) {
     for (uint32_t j = 0; j < height; j++) {
       float y = (2 * (j + 0.5) / height - 1) * cameraFOV;
 
-      for (uint32_t i = 0; i < width; i++) {
-        float x = (2 * (i + 0.5) / width - 1) * aspect * cameraFOV;
+      for (uint32_t i = 0; i < width; i += simdlen) {
+        float_v is {};
 
-        glm::vec3 ray_direction = glm::normalize(glm::vec4(x, y, -1, 0) * view);
-
-        float tmin, tmax;
-        intersect_aabb_ray(origin, ray_direction, volume_min, volume_max, tmin, tmax);
-
-        glm::vec4 dst(0.f);
-
-        constexpr float stepsize = 0.1f;
-
-        while (tmin <= tmax) {
-          float step = std::min(stepsize, tmax - tmin);
-          glm::vec3 v = glm::vec3(origin + ray_direction * (tmin + step / 2.f)) + 0.5f;
-
-          float value = sampler(v.x, v.y, v.z);
-
-          glm::vec4 src = transfer.color(value);
-
-          src.a = 1.f - std::pow(1.f - src.a, step);
-
-          src.r *= src.a;
-          src.g *= src.a;
-          src.b *= src.a;
-
-          // Evaluate the current opacity
-          dst += (1 - dst.a) * src;
-
-          tmin += stepsize;
+        for (uint32_t k = 0; k < simdlen; k++) {
+          is[k] = float(i + k);
         }
 
-        raster[(j * width + i) * 3 + 0] = (dst.r + (1 - dst.a)) * 255;
-        raster[(j * width + i) * 3 + 1] = (dst.g + (1 - dst.a)) * 255;
-        raster[(j * width + i) * 3 + 2] = (dst.b + (1 - dst.a)) * 255;
+        float_v xs = (2 * (is + 0.5f) / float(width) - 1) * aspect * cameraFOV;
+
+        glm::vec<3, float_v> ray_directions {};
+
+        for (std::size_t k = 0; k < simdlen; k++) {
+          glm::vec4 direction = glm::normalize(glm::vec4(xs[k], y, -1, 0) * view);
+          ray_directions.x[k] = direction.x;
+          ray_directions.y[k] = direction.y;
+          ray_directions.z[k] = direction.z;
+        }
+
+        float_v tmins, tmaxs;
+        intersect_aabb_rays_single_origin(origin, ray_directions, volume_min, volume_max, tmins, tmaxs);
+
+        glm::vec<4, float_v> dsts(0.f);
+
+        for (float_m mask = tmins <= tmaxs; !mask.isEmpty(); mask = tmins <= tmaxs) {
+          float_v steps = Vc::min(stepsize, tmaxs - tmins);
+
+          glm::vec<3, float_v> vs = glm::vec<3, float_v>(origin) + ray_directions * (tmins + steps / 2.f) + float_v(0.5f);
+
+          float_v values = sampler(vs.x, vs.y, vs.z, mask);
+
+          glm::vec<4, float_v> srcs {};
+          for (std::size_t k = 0; k < simdlen; k++) {
+            glm::vec4 src = transfer.color(values[k]);
+            srcs.x[k] = src.x;
+            srcs.y[k] = src.y;
+            srcs.z[k] = src.z;
+            srcs.w[k] = src.w;
+          }
+
+          srcs.a *= steps;
+
+          srcs.r *= srcs.a;
+          srcs.g *= srcs.a;
+          srcs.b *= srcs.a;
+
+          // Evaluate the current opacity
+          glm::vec<4, float_v> tmp = (1 - dsts.a) * srcs;
+          dsts.x(mask) += tmp.x;
+          dsts.y(mask) += tmp.y;
+          dsts.z(mask) += tmp.z;
+          dsts.w(mask) += tmp.w;
+
+          tmins += stepsize;
+        }
+
+        float_v rs = (dsts.r + (1 - dsts.a)) * 255;
+        float_v gs = (dsts.g + (1 - dsts.a)) * 255;
+        float_v bs = (dsts.b + (1 - dsts.a)) * 255;
+
+        rs.scatter(raster.data() + (j * width + i) * 3 + 0, offsets);
+        gs.scatter(raster.data() + (j * width + i) * 3 + 1, offsets);
+        bs.scatter(raster.data() + (j * width + i) * 3 + 2, offsets);
       }
     }
 
