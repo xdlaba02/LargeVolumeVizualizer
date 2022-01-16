b815d8ae00471d17e969c0997f10c08b6d59e49c
Drahomír Dlabaja <xdlaba02@stud.fit.vutbr.cz>
2022-01-16 11:41:05 +0100

    universal raw volume for all data types

--- src/raw_volume.h
+++ src/raw_volume.h
@@ -10,26 +10,27 @@
 #include <cstdint>
 #include <cstddef>
 
+#include <bit>
+#include <algorithm>
+
+template <typename T>
 class RawVolume {
 public:
   RawVolume() = default;
 
-  RawVolume(const char *file_name, uint64_t width, uint64_t height, uint64_t depth, uint32_t bytes_per_voxel = 1):
+  RawVolume(const char *file_name, uint64_t width, uint64_t height, uint64_t depth):
       m_width(width),
       m_height(height),
       m_depth(depth),
-      m_bytes_per_voxel(bytes_per_voxel) {
-
-    if (bytes_per_voxel != 1 && bytes_per_voxel != 2) {
-      return;
-    }
+      m_stride(width * height),
+      m_size(width * height * depth * sizeof(T)) {
 
     int fd = open(file_name, O_RDONLY);
     if (fd < 0) {
       return;
     }
 
-    m_data = mmap(nullptr, width * height * depth * bytes_per_voxel, PROT_READ, MAP_SHARED, fd, 0);
+    m_data = static_cast<T *>(mmap(nullptr, m_size, PROT_READ, MAP_SHARED, fd, 0));
 
     close(fd);
 
@@ -41,32 +42,45 @@ public:
 
   ~RawVolume() {
     if (m_data) {
-      munmap(m_data, m_width * m_height * m_depth * m_bytes_per_voxel);
+      munmap(m_data, m_size);
     }
   }
 
-  const void *data() const { return m_data; }
+  const T *data() const { return m_data; }
   operator bool() const { return m_data; }
 
-  uint32_t width()  const { return m_width; }
-  uint32_t height() const { return m_height; }
-  uint32_t depth()  const { return m_depth; }
+  constexpr uint32_t width()  const { return m_width; }
+  constexpr uint32_t height() const { return m_height; }
+  constexpr uint32_t depth()  const { return m_depth; }
 
-  uint16_t operator()(uint32_t x, uint32_t y, uint32_t z) const {
-    switch (m_bytes_per_voxel) {
-      case 1:
-        return static_cast<uint8_t *>(m_data)[(z * m_height + y) * m_width + x];
-      case 2:
-        return static_cast<uint16_t *>(m_data)[(z * m_height + y) * m_width + x];
-      default:
-        return 0;
+  constexpr uint32_t xStride() const { return 1; }
+  constexpr uint32_t yStride() const { return m_width; }
+  constexpr uint32_t zStride() const { return m_stride; }
+
+  T operator()(uint32_t x, uint32_t y, uint32_t z) const {
+    return (*this)[z * m_stride + y * m_width + x];
+  }
+
+  T operator[](uint64_t i) const {
+    if constexpr (std::endian::native == std::endian::big) {
+      T value = m_data[i];
+      uint8_t *bytes = reinterpret_cast<uint8_t *>(&value);
+      std::reverse(bytes, bytes + sizeof(T));
+      return value;
+    }
+
+    else if constexpr (std::endian::native == std::endian::little) {
+      return m_data[i];
     }
   }
 
 private:
-  void  *m_data = nullptr;
+  T *m_data = nullptr;
+
   uint32_t m_width;
   uint32_t m_height;
   uint32_t m_depth;
-  uint32_t m_bytes_per_voxel;
+
+  uint64_t m_stride;
+  uint64_t m_size;
 };
