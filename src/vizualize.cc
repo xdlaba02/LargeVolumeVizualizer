2566a62b5c075fdc8529673291e096b922742f29
Drahomír Dlabaja <xdlaba02@stud.fit.vutbr.cz>
2022-01-23 19:41:42 +0100

    Separe model matrix and view matrix

--- src/vizualize.cc
+++ src/vizualize.cc
@@ -10,12 +10,13 @@
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/matrix_inverse.hpp>
+
+#include <cmath>
 
 #include <iostream>
-#include <fstream>
-#include <sstream>
 #include <vector>
-#include <cmath>
+#include <chrono>
 
 // TODO pyramid
 // TODO min max tree
@@ -222,9 +223,7 @@ int main(int argc, char *argv[]) {
 
   Window window(1920, 1080, "Volumetric Vizualizer");
 
-  float time = 0.f;
-  constexpr float delta_time = 0.1f;
-  constexpr float camera_speed = 2.5f * delta_time;
+  auto prev_time = std::chrono::steady_clock::now();
 
   glm::vec2 prev_pos;
   window.getCursor(prev_pos.x, prev_pos.y);
@@ -232,14 +231,25 @@ int main(int argc, char *argv[]) {
   float yaw = -90;
   float pitch = 0;
 
-  glm::vec3 camera_pos   = glm::vec3(0.0f, 0.0f, 3.0f);
+  glm::vec3 camera_pos   = glm::vec3(0.0f, 0.0f, 10.0f);
   glm::vec3 camera_front = glm::vec3(0.0f, 0.0f, -1.0f);
   glm::vec3 camera_up    = glm::vec3(0.0f, 1.0f, 0.0f);
+
+  glm::vec3 volume_pos   = glm::vec3(0.0f, 0.0f, 0.0f);
+  glm::vec3 volume_scale = glm::vec3(1.0f, 1.0f, 1.0f);
 
   constexpr float yFOV = std::tan(45 * M_PI / 180 * 0.5);
   float aspect = float(window.width()) / float(window.height());
 
+  float t = 0.f;
   while (!window.shouldClose()) {
+
+    auto time = std::chrono::steady_clock::now();
+    float delta = std::chrono::duration_cast<std::chrono::milliseconds>(time - prev_time).count() / 1000.f;
+    std::cerr << delta << "\n";
+    prev_time = time;
+
+    t += delta;
 
     {
       glm::vec2 pos;
@@ -266,28 +276,61 @@ int main(int argc, char *argv[]) {
     }
 
     {
+      constexpr float camera_speed = 1.f;
+
       if (window.getKey(GLFW_KEY_ESCAPE) == GLFW_PRESS) {
         window.shouldClose(true);
       }
 
       if (window.getKey(GLFW_KEY_W) == GLFW_PRESS) {
-        camera_pos += camera_speed * camera_front;
+        camera_pos += camera_speed * delta * camera_front;
       }
 
       if (window.getKey(GLFW_KEY_S) == GLFW_PRESS) {
-        camera_pos -= camera_speed * camera_front;
+        camera_pos -= camera_speed * delta * camera_front;
       }
 
       if (window.getKey(GLFW_KEY_A) == GLFW_PRESS) {
-        camera_pos -= glm::normalize(glm::cross(camera_front, camera_up)) * camera_speed;
+        camera_pos -= glm::normalize(glm::cross(camera_front, camera_up)) * camera_speed * delta;
       }
 
       if (window.getKey(GLFW_KEY_D) == GLFW_PRESS) {
-        camera_pos += glm::normalize(glm::cross(camera_front, camera_up)) * camera_speed;
+        camera_pos += glm::normalize(glm::cross(camera_front, camera_up)) * camera_speed * delta;
+      }
+
+      if (window.getKey(GLFW_KEY_Q) == GLFW_PRESS) {
+        volume_pos -= glm::vec3(1.f, 0.f, 0.f) * delta;
+      }
+
+      if (window.getKey(GLFW_KEY_E) == GLFW_PRESS) {
+        volume_pos += glm::vec3(1.f, 0.f, 0.f) * delta;
+      }
+
+      if (window.getKey(GLFW_KEY_UP) == GLFW_PRESS) {
+        volume_scale.x += 0.1f * delta;
+      }
+
+      if (window.getKey(GLFW_KEY_DOWN) == GLFW_PRESS) {
+        volume_scale.x -= 0.1f * delta;
+      }
+
+      if (window.getKey(GLFW_KEY_RIGHT) == GLFW_PRESS) {
+        volume_scale.y += 0.1 * delta;
+      }
+
+      if (window.getKey(GLFW_KEY_LEFT) == GLFW_PRESS) {
+        volume_scale.y -= 0.1 * delta;
       }
     }
 
+    // object space - volume in 3D interval <0, volume_size - 1>
+    // normalized object space - volume in 3D interval <-0.5, 0.5>
+
+    glm::mat4 model = glm::translate(glm::mat4(1.f), volume_pos) * glm::rotate(glm::mat4(1.f), t, glm::vec3(0.f, 1.f, 0.f)) * glm::rotate(glm::mat4(1.f), glm::radians(90.f), glm::vec3(1.f, 0.f, 0.f)) * glm::scale(glm::mat4(1.f), volume_scale);
+    glm::mat4 model_inverse = glm::inverse(model);
+
     glm::mat4 view = glm::lookAt(camera_pos, camera_pos + camera_front, camera_up);
+    glm::mat4 view_inverse = glm::inverse(view);
 
     #pragma omp parallel for schedule(dynamic)
     for (uint32_t j = 0; j < window.height(); j++) {
@@ -297,17 +340,18 @@ int main(int argc, char *argv[]) {
         simd::float_v is = simd::float_v::IndexesFromZero() + i;
         simd::float_v xs = (2 * (is + 0.5f) / float(window.width()) - 1) * aspect * yFOV;
 
+        glm::vec3 ray_origin = model_inverse * glm::vec4(camera_pos, 1); // camera_pos is in world space, transform to normalized model space
         glm::vec<3, simd::float_v> ray_directions {};
 
         for (uint32_t k = 0; k < simd::len; k++) {
-          glm::vec4 direction = glm::normalize(glm::vec4(xs[k], y, -1, 0) * view);
+          glm::vec4 direction = glm::normalize(model_inverse * view_inverse * glm::vec4(xs[k], y, -1, 0)); // generate ray in view space and transform to world space
           ray_directions.x[k] = direction.x;
           ray_directions.y[k] = direction.y;
           ray_directions.z[k] = direction.z;
         }
 
         simd::float_v tmins, tmaxs;
-        intersect_aabb_rays_single_origin(camera_pos, ray_directions, {-.5, -.5, -.5}, {+.5, +.5, +.5}, tmins, tmaxs);
+        intersect_aabb_rays_single_origin(ray_origin, ray_directions, {-.5, -.5, -.5}, {+.5, +.5, +.5}, tmins, tmaxs);
 
         glm::vec<4, simd::float_v> dsts(0.f, 0.f, 0.f, 1.f);
 
@@ -316,13 +360,13 @@ int main(int argc, char *argv[]) {
         simd::float_v prev_values {};
 
         {
-          glm::vec<3, simd::float_v> vs = glm::vec<3, simd::float_v>(camera_pos) + ray_directions * tmins + simd::float_v(0.5f);
+          glm::vec<3, simd::float_v> vs = glm::vec<3, simd::float_v>(ray_origin) + ray_directions * tmins + simd::float_v(0.5f); // add 0.5 in all dimensions to transform from normalized object space to
           prev_values = blockedVolumeSampler(blocked_volume, vs.x, vs.y, vs.z, tmins <= tmaxs);
           tmins += stepsize;
         }
 
         for (simd::float_m mask = tmins <= tmaxs; !mask.isEmpty(); mask &= tmins <= tmaxs) {
-          glm::vec<3, simd::float_v> vs = glm::vec<3, simd::float_v>(camera_pos) + ray_directions * tmins + simd::float_v(0.5f);
+          glm::vec<3, simd::float_v> vs = glm::vec<3, simd::float_v>(ray_origin) + ray_directions * tmins + simd::float_v(0.5f); // add 0.5 in all dimensions to transform from normalized object space to
 
           simd::float_v values = blockedVolumeSampler(blocked_volume, vs.x, vs.y, vs.z, mask);
 
@@ -366,7 +410,5 @@ int main(int argc, char *argv[]) {
 
     window.swapBuffers();
     window.pollEvents();
-
-    time += delta_time;
   }
 }
