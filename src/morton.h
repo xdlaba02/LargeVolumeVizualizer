00659b09f243f43154d51705dc891b1832d3f91a
Drahomír Dlabaja <xdlaba02@stud.fit.vutbr.cz>
2022-02-13 00:44:16 +0100

    Fix morton for SIMD

--- src/morton.h
+++ src/morton.h
@@ -4,7 +4,7 @@
 
 #include <bit>
 
-template <size_t BITS>
+template <uint32_t BITS>
 class Morton {
 public:
     template <typename T>
@@ -29,9 +29,9 @@ public:
 
     template <typename T>
     static inline constexpr void separate_interleaved(T v, T &x, T &y, T &z) {
-      x = (v >> 0) & magic_number<T>(1);
-      y = (v >> 1) & magic_number<T>(1);
-      z = (v >> 2) & magic_number<T>(1);
+      x = (v >> 0) & magic_number(1);
+      y = (v >> 1) & magic_number(1);
+      z = (v >> 2) & magic_number(1);
     }
 
     template <typename T>
@@ -44,34 +44,33 @@ public:
 
 private:
 
-  template <typename T>
-  static inline consteval T magic_number(T bits) {
-    T result = (1 << bits) - 1;
-    for (T shift = bits * 3; shift < T(sizeof(T) * 8); shift <<= 1) {
+  static inline consteval uint32_t magic_number(uint32_t bits) {
+    uint32_t result = (1 << bits) - 1;
+    for (uint32_t shift = bits * 3; shift < 32; shift <<= 1) {
       result |= result << shift;
     }
     return result;
   }
 
-  template <size_t WIDTH, typename T, typename std::enable_if_t<WIDTH == 1, bool> = true>
+  template <uint32_t WIDTH, typename T, typename std::enable_if_t<WIDTH == 1, bool> = true>
   static inline constexpr T interleaver(T v) {
-    return (v | (v << (1 << WIDTH))) & magic_number<T>(1 << (WIDTH - 1));
+    return (v | (v << (1 << WIDTH))) & magic_number(1 << (WIDTH - 1));
   }
 
-  template <size_t WIDTH, typename T, typename std::enable_if_t<WIDTH != 1, bool> = true>
+  template <uint32_t WIDTH, typename T, typename std::enable_if_t<WIDTH != 1, bool> = true>
   static inline constexpr T interleaver(T v) {
-    v = (v | (v << (1 << WIDTH))) & magic_number<T>(1 << (WIDTH - 1));
+    v = (v | (v << (1 << WIDTH))) & magic_number(1 << (WIDTH - 1));
     return interleaver<WIDTH - 1, T>(v);
   }
 
-  template <size_t WIDTH, typename T, typename std::enable_if_t<WIDTH == 1, bool> = true>
+  template <uint32_t WIDTH, typename T, typename std::enable_if_t<WIDTH == 1, bool> = true>
   static inline constexpr T deinterleaver(T v) {
-    return (v | (v >> (1 << WIDTH))) & magic_number<T>(1 << WIDTH);
+    return (v | (v >> (1 << WIDTH))) & magic_number(1 << WIDTH);
   }
 
-  template <size_t WIDTH, typename T, typename std::enable_if_t<WIDTH != 1, bool> = true>
+  template <uint32_t WIDTH, typename T, typename std::enable_if_t<WIDTH != 1, bool> = true>
   static inline constexpr T deinterleaver(T v) {
     v = deinterleaver<WIDTH - 1, T>(v);
-    return (v | (v >> (1 << WIDTH))) & magic_number<T>(1 << WIDTH);
+    return (v | (v >> (1 << WIDTH))) & magic_number(1 << WIDTH);
   }
 };
