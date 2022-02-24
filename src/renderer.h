228cedd58314ed265bd1d0db0dc1569e67cdec3e
Drahomír Dlabaja <xdlaba02@stud.fit.vutbr.cz>
2022-02-24 19:04:13 +0100

    core prototype

--- src/renderer.h
+++ src/renderer.h
@@ -21,101 +21,67 @@ struct RayRange {
 };
 
 struct Ray {
-  Ray(const glm::vec3 &origin, const glm::vec3 &direction)
-      : origin(origin)
-      , direction(direction)
-      , direction_inverse(1.f / direction)
-  {
-    std::array<float, 8> tmins {};
+  Ray(const glm::vec3 &origin, const glm::vec3 &direction): origin(origin), direction(direction), direction_inverse(1.f / direction) {}
 
-    static constinit std::array<glm::vec3, 3> points { glm::vec3{ 0.0f, 0.0f, 0.0f },
-                                                       glm::vec3{ 0.5f, 0.5f, 0.5f },
-                                                       glm::vec3{ 1.0f, 1.0f, 1.0f } };
-
-    for (uint8_t z: {0, 1}) {
-      for (uint8_t y: {0, 1}) {
-        for (uint8_t x: {0, 1}) {
-          uint8_t i = z << 2 | y << 1 | x;
-
-          glm::vec3 min = { points[x + 0].x, points[y + 0].y, points[z + 0].z };
-          glm::vec3 max = { points[x + 1].x, points[y + 1].y, points[z + 1].z };
-
-          float dummy_max;
-          intersect_aabb_ray(origin, direction_inverse, min, max, tmins[i], dummy_max);
-
-          tmins[i] = -std::numeric_limits<float>::infinity();
-
-          for (uint32_t i = 0; i < 3; ++i) {
-            float t0 = (min[i] - origin[i]) * direction_inverse[i];
-            float t1 = (max[i] - origin[i]) * direction_inverse[i];
-
-            if (ray_direction_inverse[i] < 0.f) {
-              tmin = std::max(tmin, t1);
-              tmax = std::min(tmax, t0);
-            }
-            else {
-              tmin = std::max(tmin, t0);
-              tmax = std::min(tmax, t1);
-            }
-          }
-        }
-      }
-    }
-
-    std::iota(std::begin(octant_order), std::end(octant_order), 0);
-    std::sort(std::begin(octant_order), std::end(octant_order), [&](uint8_t l, uint8_t r) { return tmins[l] < tmins[r]; });
-  }
-
-  RayRange reduceRange(uint8_t octant, const RayRange &range, const glm::vec3 &tcenter) const {
-    Range octant_range = range;
-
-    for (uint8_t d: {0, 1, 2}) {
-      usint8_t pos = octant_order[octant] & (1 << d);
-      uint8_t near = direction[d] < 0;
-
-      if (pos == near) {
-        octant_range.max = std::min(octant_range.max, tcenter[d]);
-      }
-      else {
-        octant_range.min = std::max(octant_range.min, tcenter[d]);
-      }
-    }
-
-    return octant_range;
-  }
-
-  glm::vec3 origin;
-  glm::vec3 direction;
-  glm::vec3 direction_inverse;
-
-private:
-  std::array<uint8_t, 8> octant_order;
+  const glm::vec3 origin;
+  const glm::vec3 direction;
+  const glm::vec3 direction_inverse;
 };
 
+// Interactive isosurface ray tracing of large octree volumes
+// https://www.researchgate.net/publication/310054812_Interactive_isosurface_ray_tracing_of_large_octree_volumes
 template <typename T, typename F>
-void recursive_integrate(const BlockedVolume<T> &volume, const Ray &ray, const RayRange &range, const glm::vec3 &center, uint32_t layer, F &callback) {
-  if (layer == std::size(volume.layers) - 1) {
+void recursive_integrate(const Ray &ray, const RayRange &range, glm::vec<3, uint32_t> cell, uint32_t layer, F &callback) {
+  if (callback(range, cell >> layer /* FIXME this might be off one bit error*/, layer)) {
+    uint32_t child_bit = 1 << layer;
 
-    float stepsize = m_stepsize * (std::size(volume.layers) - layer);
+    glm::vec<3, uint32_t> center = cell | child_bit;
 
-    for (float t = range.min; t < range.max; t += stepsize) {
-      glm::vec3 sample = ray.origin + ray.direction * tmin;
+    glm::vec3 penter = ray.origin + ray.direction * range.min;
 
-      callback(volume.sample_volume(sample.x, sample.y, sample.z, layer));
-    }
-  }
-  else {
-    // find collisions of the ray with three center planes
-    glm::vec3 tcenter = (center - ray.origin) * ray.direction_inverse;
+    glm::vec<3, uint32_t> child_cell = cell;
 
-    // Find ranges of t for suboctants, recurse if intersecting.
-    for (uint8_t i = 0; i < 8; i++) {
-      // This is faster than ray-bb intersection. It reduces existing range.
-      Range octant_range = ray.reduceRange(i, range, tcenter);
+    child_cell.x |= penter.x > center.x ? child_bit : 0;
+    child_cell.y |= penter.y > center.y ? child_bit : 0;
+    child_cell.z |= penter.z > center.z ? child_bit : 0;
 
-      if (octant_range.min < octant_range.max) {
-        recursive_integrate(volume, ray, octant_range, /*TODO*/, layer + 1, dst);
+    glm::vec3 tcenter = (glm::vec3(center) - ray.origin) * ray.direction_inverse;
+
+    // fast sort axis by tcenter
+    std::array<uint8_t, 3> axis;
+
+#if 1
+    uint8_t order = ((tcenter[0] < tcenter[1]) << 2)
+                  | ((tcenter[0] < tcenter[2]) << 1)
+                  | ((tcenter[1] < tcenter[2]) << 0);
+
+    axis[0] = std::array<uint8_t, 8>{ 2, 1, 0, 1, 2, 0, 0, 0 }[order];
+    axis[1] = std::array<uint8_t, 8>{ 1, 2, 0, 0, 0, 1, 2, 1 }[order];
+    axis[2] = std::array<uint8_t, 8>{ 0, 0, 0, 2, 1, 2, 1, 2 }[order];
+#else
+    bool gt01 = tcenter[0] > tcenter[1];
+    bool gt02 = tcenter[0] > tcenter[2];
+    bool gt12 = tcenter[1] > tcenter[2];
+
+    axis[ gt01 +  gt02] = 0;
+    axis[!gt01 +  gt12] = 1;
+    axis[!gt02 + !gt12] = 2;
+#endif
+
+    float tmin = range.min;
+    for (uint8_t i = 0; i < 3; i++) {
+      float tmax = tcenter[axis[i]];
+
+      if (tmin < tmax) {
+        recursive_integrate(ray, { tmin, tmax }, child_cell, layer - 1, callback);
+        tmin = tmax;
+
+        child_cell[axis[i]] ^= child_bit;
       }
+    }
+
+    if (tmin < range.max) {
+      recursive_integrate(ray, { tmin, range.max }, child_cell, layer - 1, callback);
     }
   }
 }
@@ -123,8 +89,12 @@ void recursive_integrate(const BlockedVolume<T> &volume, const Ray &ray, const R
 template <typename T, typename F>
 void render(const BlockedVolume<T> &volume, const glm::mat4 &mv, uint32_t width, uint32_t height, float yfov_degrees, const F &output) {
 
-  // This transfroms ray from cannocical clip space to normalized object space where volume is inside boundig box (0, 0, 0), (1, 1, 1)
-  glm::mat4 ray_transform = glm::translate(glm::mat4(1.f), glm::vec3(0.5f, 0.5f, 0.5f)) * glm::inverse(mv);
+  glm::vec3 size_in_blocks { volume.info.layers[0].width_in_blocks,
+                             volume.info.layers[0].height_in_blocks,
+                             volume.info.layers[0].depth_in_blocks };
+
+  // This transfroms ray from cannocical clip space to object space where volume is inside boundig box (0, 0, 0), (width_in_blocks, height_in_blocks, depth_in_blocks)
+  glm::mat4 ray_transform = glm::scale(glm::mat4(1.f), size_in_blocks) * glm::translate(glm::mat4(1.f), glm::vec3(0.5f, 0.5f, 0.5f)) * glm::inverse(mv);
 
   glm::vec3 origin = ray_transform * glm::vec4(0.f, 0.f, 0.f, 1.f);
 
@@ -148,14 +118,15 @@ void render(const BlockedVolume<T> &volume, const glm::mat4 &mv, uint32_t width,
 
       Ray ray(origin, ray_transform * glm::normalize(glm::vec4(x, y, -1.f, 0.f)));
 
-      RayRange range {};
+      float tmin {};
+      float tmax {};
 
-      intersect_aabb_ray(ray.origin, ray.direction_inverse, {0.f, 0.f, 0.f}, {1.f, 1.f, 1.f}, range.min, range.max);
+      intersect_aabb_ray(ray.origin, ray.direction_inverse, {}, size_in_blocks, tmin, tmax);
 
       glm::vec4 dst(0.f, 0.f, 0.f, 1.f);
 
       if (tmin < tmax) {
-        recursive_integrate(volume, ray, range, {0.5f, 0.5f, 0.5f}, 0, dst);
+        recursive_integrate(ray, { tmin, tmax }, { 0, 0, 0 }, std::size(volue.layers) - 1, /* TODO output */);
       }
 
       output(i, j, dst);
@@ -164,7 +135,7 @@ void render(const BlockedVolume<T> &volume, const glm::mat4 &mv, uint32_t width,
 }
 
 bool traverse(Ray ray, int depth, uint node_index, Vec3f cell, float tenter, float texit) {
-  Vec3f center = Vec3f( cell | Vec3i((1 << (max_depth - depth - 1))) );
+  Vec3f center = Vec3f( cell | Vec3i(child_bit) );
 
   Vec3f tcenter = (center - ray.orig) / ray.dir;
 
@@ -178,9 +149,9 @@ bool traverse(Ray ray, int depth, uint node_index, Vec3f cell, float tenter, flo
 
   int child = tc.x << 2 | tc.y << 1 | tc.z;
 
-  child_cell.x |= tc.x ? (1 << (max_depth - depth - 1)) : 0;
-  child_cell.y |= tc.y ? (1 << (max_depth - depth - 1)) : 0;
-  child_cell.z |= tc.z ? (1 << (max_depth - depth - 1)) : 0;
+  child_cell.x |= tc.x ? child_bit : 0;
+  child_cell.y |= tc.y ? child_bit : 0;
+  child_cell.z |= tc.z ? child_bit : 0;
 
   Vec3i axis_isects;
 
@@ -224,11 +195,11 @@ bool traverse(Ray ray, int depth, uint node_index, Vec3f cell, float tenter, flo
 
     if (child & axisbit) {
       child &= ~axisbit;
-      child_cell[axis_isects[i]] &= ~(1 << (max_depth - depth - 1));
+      child_cell[axis_isects[i]] &= ~child_bit;
     }
     else {
       child |= axisbit;
-      child_cell[axis_isects[i]] |= (1 << (max_depth - depth - 1));
+      child_cell[axis_isects[i]] |= child_bit;
     }
   }
 
